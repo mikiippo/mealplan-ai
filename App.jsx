aa84b2204e947f8e232ba377f4224e3f869ba50b
mikiippo <ippolitomichelangelo@hotmail.com>
2025-11-30 17:41:51 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,1 @@
+export default function App(){return <h1>Dieta AI – Funziona!</h1>;}
---
+++ index.js
@@ -0,0 +1,1 @@
+const express=require('express');const app=express();app.get('/',(req,res)=>res.send('API OK'));app.listen(3001,()=>console.log('API running'));
